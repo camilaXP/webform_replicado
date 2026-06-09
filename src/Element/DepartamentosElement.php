0c5b894ed0fb00e58c19f6e45628d201953a0f4d
Camila Moraes <camila07moraes.01@gmail.com>
2026-06-09 16:38:28 -0300

    Implementa mais uma função ao webform replicado, total 4

---
+++ src/Element/DepartamentosElement.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\webform_replicado\Element;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Element\Textfield;
+
+
+
+/**
+ * Provides a department element.
+ *
+ * @FormElement("departamentos_fflch")
+ */
+
+
+class DepartamentosElement extends Textfield {
+
+  public function getInfo(): array {
+    $class = get_class($this);
+    return parent::getInfo() + [
+      '#input' => TRUE,
+      '#element_validate' => [
+        [$class, 'validateDepartamentos'],
+      ],
+    ];
+  }
+
+
+
+
+
+public static function validateDepartamentos(&$element, FormStateInterface $form_state, &$complete_form): void {
+    
+ $value = $element['#value'];
+
+ $value = mb_strtolower($element['#value'], 'UTF-8');
+
+ $value = strtr($value, [
+  'á' => 'a', 'à' => 'a', 'ã' => 'a', 'â' => 'a',
+  'é' => 'e', 'ê' => 'e',
+  'í' => 'i',
+  'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+  'ú' => 'u',
+  'ç' => 'c',
+ ]);
+
+ $departamentos = [
+  'departamento de antropologia',
+  'departamento de ciencia politica',
+  'departamento de filosofia',
+  'departamento de geografia',
+  'departamento de historia',
+  'departamento de letras classicas e vernaculas',
+  'departamento de letras modernas',
+  'departamento de letras orientais',
+  'departamento de linguistica',
+  'departamento de sociologia',
+  'departamento de teoria literaria e literatura comparada',
+  'fla',
+  'flp',
+  'flf',
+  'flg',
+  'flh',
+  'flc',
+  'flm',
+  'flo',
+  'fll',
+  'fsl',
+  'flt',
+ ];
+
+$valido = FALSE;
+
+  foreach ($departamentos as $departamento) {
+    if (str_contains($value, $departamento)) {
+      $valido = TRUE;
+      break;
+    }
+  }
+
+  if (!$valido) {
+    $form_state->setError(
+      $element,
+      t('Esse departamento não é da FFLCH.')
+    );
+  }
+  }
+}  
+ 
+  
---
+++ src/Plugin/WebformElement/DepartamentosElement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\webform_replicado\Plugin\WebformElement;
+
+use Drupal\webform\Plugin\WebformElement\TextField;
+
+/**
+ * Provides a 'departamentos_fflch' Webform element.
+ *
+ * @WebformElement(
+ *   id = "departamentos_fflch",
+ *   label = @Translation("Departamentos da FFLCH"),
+ *   category = @Translation("USP")
+ * )
+ */
+class DepartamentosElement extends TextField {
+}
